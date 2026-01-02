7dddf2c7e2b8bbcf82350a3da323e9eb3fa93b5c
Rodrigo Polydoro <rspolydoro@gmail.com>
2026-01-02 08:54:05 -0300

    Revert "Add start.sh scripts for Railway deployment"
    This reverts commit f49949e6ea46bcd013e700a5438b037d5d18bbb1.

--- retro101-backend/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Start script for Railway deployment
-
-# Run the Spring Boot application
-java -jar target/retro101-backend.jar
--- retro101-frontend/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Start script for Railway deployment
-
-# Start the Vite preview server
-npm run preview -- --port $PORT --host 0.0.0.0
